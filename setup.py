e6d93bf42b8777c89d1199362f8f29b87f011610
Wayne <wayne.cq@hotmail.com>
2017-06-21 13:12:10 +0100

    reorganized files

---
+++ setup.py
@@ -0,0 +1,24 @@
+'''
+Created on 21 Jun 2017
+
+@author: wayne
+'''
+from setuptools import setup
+
+setup(
+    name = "quant",
+    version = "1.0.1",
+    author = "Wayne",
+    author_email = "wayne.cq@hotmail.com",
+    description = ("Independent research"),
+    license = "BSD",
+    keywords = "quant",
+    url = "https://github.com/awesomewayne/quant.git",
+    packages=['quant'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
+
