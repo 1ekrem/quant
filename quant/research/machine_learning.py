522dae3916f0fba532ceb0da20699518273e2429
Wayne <wayne.cq@hotmail.com>
2017-06-23 13:08:35 +0100

    updated machine learning expriment

--- quant/research/machine_learning.py
+++ quant/research/machine_learning.py
@@ -10,6 +10,7 @@ DATABASE_NAME = 'quant'
 INDEX_TABLE_NAME = 'bloomberg_index_prices'
 US_ECON_TABLE_NAME = 'bloomberg_us_econ'
 ACTUAL_RELEASE = 'ACTUAL_RELEASE'
+PX_LAST = 'PX_LAST'
 
 
 def load_bloomberg_index_prices(ticker='SPX Index'):
@@ -17,4 +18,34 @@ def load_bloomberg_index_prices(ticker='SPX Index'):
 
 
 def load_bloomberg_econ_release(ticker, table_name=US_ECON_TABLE_NAME):
-    return tu.get_timeseries(DATABASE_NAME, table_name, column_list=[ticker + '|' + ACTUAL_RELEASE])
+    data = tu.get_timeseries(DATABASE_NAME, table_name, column_list=[ticker + '|' + ACTUAL_RELEASE])
+    if data is not None:
+        data = data.iloc[:, 0]
+        data.name = ticker
+    return data
+
+
+def load_bloomberg_econ_last(ticker, table_name=US_ECON_TABLE_NAME):
+    data = tu.get_timeseries(DATABASE_NAME, table_name, column_list=[ticker + '|' + PX_LAST])
+    if data is not None:
+        data = data.iloc[:, 0]
+        data.name = ticker
+    return data
+
+
+def load_bloomberg_econ_change(ticker, table_name=US_ECON_TABLE_NAME):
+    release = load_bloomberg_econ_release(ticker, table_name)
+    last = load_bloomberg_econ_last(ticker, table_name)
+    if release is not None and last is not None:
+        return release - last.shift()
+    else:
+        return None
+
+
+def load_bloomberg_econ_revision(ticker, table_name=US_ECON_TABLE_NAME):
+    release = load_bloomberg_econ_release(ticker, table_name)
+    last = load_bloomberg_econ_last(ticker, table_name)
+    if release is not None and last is not None:
+        return (last - release).shift()
+    else:
+        return None
